6ce2ebdd824315641c9c1b1feaed9c8674660206
douglas aleixo <douglas.aleixo2806@gmail.com>
2024-08-15 16:45:05 -0300

    create header

---
+++ front-end/src/components/header/page.tsx
@@ -0,0 +1,28 @@
+import styles from "./page.module.css"
+import Image from "next/image"
+import moon from "./images/moon.svg"
+
+export function Header() {
+    return (
+        <>
+        <header className={styles.header}>
+            <div>
+                <h1 className={styles.h1Header}> FAKE CHAT </h1>
+            </div>
+            <nav className={styles.navbar}>
+                <div>
+                    <p className={styles.pNavBar}> Modo Escuro </p>
+                </div>
+                <div>
+                    <Image
+                        src={moon}
+                        width={24}
+                        height={24}
+                        alt="Picture of a moon"
+                    />
+                </div>
+            </nav>
+        </header>
+        </>
+    )
+}
