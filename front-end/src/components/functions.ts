3c2d91c771a29641e65b8b8cd78fef4cf38720d0
PedroLarchert <larchertpedro@gmail.com>
2024-08-25 01:02:43 -0300

    fix: troca de backend

--- front-end/src/components/functions.ts
+++ front-end/src/components/functions.ts
@@ -16,20 +16,18 @@ class functions {
     }
 
     private static async sendBack(text: string) {
+        
         try {
-            const response = await axios({
-                method: "post",
-                url: "http://localhost:8080/request",
-                data: {
+            const response = await axios.post('http://127.0.0.1:5000/predict', {
                 text: text
-            }
-            });
-            
-            return response.data.responseIA;
+            })
+  
+            return response.data.result;
         } 
+
         catch (error: any) {
             const response = "Aconteceu um erro"
-
+  
             return response;
         }
     }
